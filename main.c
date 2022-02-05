c679c9485bb2739fedd4d2b4a1083d727865992b
cpasjuste <cpasjuste@gmail.com>
2022-02-05 09:54:27 +0100

    testing

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-
 #include <libk/string.h>
 #include <psp2kern/net/net.h>
 #include <psp2common/types.h>
@@ -6,11 +5,16 @@
 #include <psp2kern/kernel/threadmgr/thread.h>
 #include <psp2kern/kernel/modulemgr.h>
 #include <psp2kern/kernel/sysmem.h>
+#include <taihen.h>
+
+#define MAX_HOOKS 16
+static SceUID g_hooks[MAX_HOOKS];
+static tai_hook_ref_t ref_hooks[MAX_HOOKS];
 
 #define printf ksceDebugPrintf
 
 #define SERVER_PORT 4444
-#define BUFFER_SIZE 16
+#define BUFFER_SIZE 2048
 
 SceNetSockaddrIn client;
 unsigned int clientLen;
@@ -97,17 +101,21 @@ int cmd_thread(SceSize args, void *argp) {
 
     printf("ksceNetRecvfrom\n");
 
-    char *msg = kmalloc(BUFFER_SIZE);
-    if (msg != NULL) {
-        memset(msg, 0, BUFFER_SIZE);
-        int size = ksceNetRecvfrom(
-                client_sock, msg, BUFFER_SIZE, 0x1000, NULL, 0);
-        printf("ksceNetRecvfrom: sock: %i, size: %i (0x%08x), msg: %s\n",
-               client_sock, size, size, msg);
-        kfree(msg);
-    } else {
-        printf("kmalloc failed\n");
-    }
+    //char *msg = kmalloc(BUFFER_SIZE);
+    //if (msg != NULL) {
+    char msg[BUFFER_SIZE];
+    memset(msg, 0, BUFFER_SIZE);
+    int flags = 0x1000;
+    //flags &= ~(SCE_NET_MSG_USECRYPTO | SCE_NET_MSG_USESIGNATURE);
+    printf("flags: 0x%08x\n", flags);
+    int size = ksceNetRecvfrom(
+            client_sock, msg, BUFFER_SIZE, flags, NULL, NULL);
+    printf("ksceNetRecvfrom: sock: %i, size: %i (0x%08x), msg: %s\n",
+           client_sock, size, size, msg);
+    //kfree(msg);
+    //} else {
+    //    printf("kmalloc failed\n");
+    // }
 
     cleanup();
     ksceKernelExitDeleteThread(0);
@@ -115,7 +123,110 @@ int cmd_thread(SceSize args, void *argp) {
     return 0;
 }
 
+#if 0
+static int
+ksceNetRecvfrom_hook(int s, void *buf, unsigned int len, int flags, SceNetSockaddr *from, unsigned int *fromlen) {
+    printf("ksceNetRecvfrom: %i %p %i %x %p %p\n",
+           s, buf, len, flags, from, fromlen);
+    return TAI_CONTINUE(int, ref_hooks[3], s, buf, len, flags, from, fromlen);
+}
+#endif
+
+static int
+FUN_81007a78_hook(int s, void *buf, unsigned int len, int flags, SceNetSockaddr *from, unsigned int *fromlen, int unk) {
+
+    if (s == client_sock) {
+        printf("FUN_81007a78_hook: %i %p %i 0x%x %p %p %i\n",
+               s, buf, len, flags, from, fromlen, unk);
+        return TAI_CONTINUE(int, ref_hooks[0], s, buf, len, flags, from, fromlen, 0);
+    } else {
+        return TAI_CONTINUE(int, ref_hooks[0], s, buf, len, flags, from, fromlen, unk);
+    }
+}
+
+static int
+FUN_8100072C_hook(int s, int unk) {
+
+    int ret;
+
+    if (s == client_sock) {
+        ret = TAI_CONTINUE(int, ref_hooks[1], s, 2);
+        printf("FUN_8100072C_hook: %i %i %i\n", s, unk, ret);
+    } else {
+        ret = TAI_CONTINUE(int, ref_hooks[1], s, unk);
+    }
+
+    return ret;
+}
+
+static int
+FUN_8100676c_hook(int s, int *piVar2, unsigned int *fromlen, int *local_24, int unk2) {
+
+    int ret;
+
+    if (s == client_sock) {
+        printf("FUN_8100676c_hook: %i %i\n", piVar2[4], piVar2[3]);
+        //piVar2[3] = 1;
+        //piVar2[4] = 0;
+        //local_24[0] = 1;
+        ret = TAI_CONTINUE(int, ref_hooks[2], s, piVar2, fromlen, local_24, unk2);
+        printf("FUN_8100676c_hook: %i, %p, %i, %p, %i, %i (buf: %s, size: %i)\n",
+               s, piVar2, fromlen, local_24, unk2, ret, piVar2[7], piVar2[8]);
+    } else {
+        ret = TAI_CONTINUE(int, ref_hooks[2], s, piVar2, fromlen, local_24, unk2);
+    }
+
+    return ret;
+}
+
 int module_start(SceSize argc, const void *args) {
+
+    tai_module_info_t tai_info;
+    tai_info.size = sizeof(tai_module_info_t);
+    int res = taiGetModuleInfoForKernel(KERNEL_PID, "SceNetPs", &tai_info);
+    printf("taiGetModuleInfoForKernel: %i\n", res);
+
+    g_hooks[0] = taiHookFunctionOffsetForKernel(
+            KERNEL_PID,
+            &ref_hooks[0],
+            tai_info.modid,
+            0,
+            0x7a78,
+            1,
+            FUN_81007a78_hook);
+    printf("hook: FUN_81007a78: 0x%08X\n", g_hooks[0]);
+
+    g_hooks[1] = taiHookFunctionOffsetForKernel(
+            KERNEL_PID,
+            &ref_hooks[1],
+            tai_info.modid,
+            0,
+            0x72c,
+            1,
+            FUN_8100072C_hook);
+    printf("hook: FUN_81007a78: 0x%08X\n", g_hooks[1]);
+
+    g_hooks[2] = taiHookFunctionOffsetForKernel(
+            KERNEL_PID,
+            &ref_hooks[2],
+            tai_info.modid,
+            0,
+            0x676c,
+            1,
+            FUN_8100676c_hook);
+    printf("hook: FUN_8100676c: 0x%08X\n", g_hooks[2]);
+
+#if 0
+    g_hooks[3] = taiHookFunctionExportForKernel(
+            KERNEL_PID,
+            &ref_hooks[3],
+            "SceNetPs",
+            0xB2A5C920, // SceNetPsForDriver
+            0x49B1669C, // ksceNetRecvfrom
+            ksceNetRecvfrom_hook);
+    //LOG("hook: _printf2: 0x%08X\n", g_hooks[3]);
+#endif
+
     cmd_thid = ksceKernelCreateThread("knet", cmd_thread, 64, 0x2000, 0, 0, 0);
     if (cmd_thid >= 0) {
         ksceKernelStartThread(cmd_thid, 0, NULL);
@@ -125,6 +236,10 @@ int module_start(SceSize argc, const void *args) {
 }
 
 int module_stop(SceSize argc, const void *args) {
+    for (int i = 0; i < MAX_HOOKS; i++) {
+        if (g_hooks[i] >= 0)
+            taiHookReleaseForKernel(g_hooks[i], ref_hooks[i]);
+    }
     return SCE_KERNEL_STOP_SUCCESS;
 }
 
